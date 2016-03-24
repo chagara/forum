1e84713f5382d7a09f41e8b7de804dac85bb8d0b
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-03-24 22:38:30 +0000

    Set up relationship between section and categories

--- forum/models.py
+++ forum/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Section(models.Model):
 	name = models.CharField(default='Section', max_length=32)
+
+
+class Category(models.Model):
+	name = models.CharField(default='Category', max_length=124)
+	description = models.CharField(default='', max_length=124)
+	section = models.ForeignKey(Section, null=True)
--- forum/tests.py
+++ forum/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Section
+from .models import Category, Section
 
 # Create your tests here.
 class ModelsTest(TestCase):
@@ -8,3 +8,18 @@ class ModelsTest(TestCase):
 		first_section = Section.objects.create(name="Section1")
 		saved_section = Section.objects.get(name="Section1")
 		self.assertEquals(first_section.name, saved_section.name)
+
+	def test_category_model_has_correct_attributes(self):
+		section = Section.objects.create(name="Section")
+		first_category = Category.objects.create(
+			name="Category1",
+			description="description",
+			section=section)
+		saved_category = Category.objects.get(name="Category1")
+		self.assertEquals(first_category.name, saved_category.name)
+
+	def test_each_category_belongs_to_a_section(self):
+		section = Section.objects.create(name="Section")
+		category = Category.objects.create(name="Category", section=section)
+		saved_category = Category.objects.get(name="Category")
+		self.assertEquals(saved_category.section, section)
