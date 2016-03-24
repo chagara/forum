32bd3e4aaab287d92b3877784b105e3b914c9e6f
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-03-24 22:07:25 +0000

    Wrote a test for the Sections model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Section(models.Model):
+	name = models.CharField(default='Section', max_length=32)
--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Section
 
 # Create your tests here.
+class ModelsTest(TestCase):
+
+	def test_section_model_has_correct_attributes(self):
+		first_section = Section.objects.create(name="Section1")
+		saved_section = Section.objects.get(name="Section1")
+		self.assertEquals(first_section.name, saved_section.name)
