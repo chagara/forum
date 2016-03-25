1023a53c9b197c445301b307077f950b677bc92c
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-03-25 13:50:32 +0000

    Each thread now belongs to a category

--- forum/tests.py
+++ forum/tests.py
@@ -28,10 +28,22 @@ class ModelsTest(TestCase):
 
     def test_thread_model_has_correct_attributes(self):
         datetime_posted = timezone.now()
+        category = Category.objects.create()
         first_thread = Thread.objects.create(
             name="The first thread",
-            datetime_posted=timezone.now())
+            datetime_posted=datetime_posted,
+            category=category)
         saved_thread = Thread.objects.first()
 
         self.assertEquals(first_thread, saved_thread)
         self.assertEquals(datetime_posted, saved_thread.datetime_posted)
+
+    def test_each_thread_belongs_to_a_category(self):
+        section = Section.objects.create()
+        category = Category.objects.create(name="A category", section=section)
+        thread = Thread.objects.create(
+            name="Thread",
+            datetime_posted=timezone.now(),
+            category=category)
+
+        self.assertEquals(thread.category, category)
