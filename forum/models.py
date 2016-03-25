9768a076003da0508bfd446e5229f5cc9df75537
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-03-25 12:42:21 +0000

    Changed indentation to use spaces rather than tabs

--- forum/models.py
+++ forum/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Section(models.Model):
-	name = models.CharField(default='Section', max_length=32)
+    name = models.CharField(default='Section', max_length=32)
 
 
 class Category(models.Model):
-	name = models.CharField(default='Category', max_length=124)
-	description = models.CharField(default='', max_length=124)
-	section = models.ForeignKey(Section, null=True)
+    name = models.CharField(default='Category', max_length=124)
+    description = models.CharField(default='', max_length=124)
+    section = models.ForeignKey(Section, null=True)
