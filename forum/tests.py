8d978ad641cac3cf440e4aca1400ee43f8796655
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-04-09 18:11:02 +0100

    Removed unecessary tests. The link between comments and threads etc are already being tested for in the attribute tests.

--- forum/tests.py
+++ forum/tests.py
@@ -18,11 +18,6 @@ class ModelsTest(TestCase):
             section=section)
         self.assertEquals(first_category.name, "Category1")
 
-    def test_each_category_belongs_to_a_section(self):
-        section = Section(name="Section")
-        category = Category(name="Category", section=section)
-        self.assertEquals(category.section, section)
-
     def test_thread_model_has_correct_attributes(self):
         datetime_posted = timezone.now()
         section = Section()
@@ -34,16 +29,6 @@ class ModelsTest(TestCase):
 
         self.assertEquals(first_thread.name, "The first thread")
         self.assertEquals(datetime_posted, first_thread.datetime_posted)
-
-    def test_each_thread_belongs_to_a_category(self):
-        section = Section()
-        category = Category(name="A category", section=section)
-        thread = Thread(
-            name="Thread",
-            datetime_posted=timezone.now(),
-            category=category)
-
-        self.assertEquals(thread.category, category)
 
     def test_comment_model_has_correct_attributes(self):
         datetime_posted = timezone.now()
@@ -62,19 +47,6 @@ class ModelsTest(TestCase):
         self.assertEquals(comment.author, "Someone")
         self.assertEquals(
             comment.datetime_posted, datetime_posted)
-
-    def test_each_comment_belongs_to_a_thread(self):
-        section = Section()
-        category = Category(section=section)
-        thread = Thread(
-            datetime_posted=timezone.now(),
-            category=category)
-        comment = Comment(
-            text="Some text",
-            author="Someone",
-            datetime_posted=timezone.now(),
-            thread=thread)
-
         self.assertEquals(comment.thread, thread)
 
 
