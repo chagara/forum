b92aceda379984f77392eaaef3be3d2cc24895e6
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-04-16 17:44:24 +0100

    Set up the static folder. Added jquery-ui and bootstrap

--- djangolearners/settings.py
+++ djangolearners/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -121,5 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
