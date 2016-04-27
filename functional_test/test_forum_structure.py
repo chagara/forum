88e599af2550fb23306dcd7001a69a1d299d4f0c
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-04-27 15:25:48 +0100

    Wrote a basic user story

--- functional_test/test_forum_structure.py
+++ functional_test/test_forum_structure.py
@@ -1,7 +1,32 @@
 from functional_test.base import FunctionalTest
 
 
-class CreateAPostTest(FunctionalTest):
+class ForumStructureTest(FunctionalTest):
 
-    def test_it(self):
+    def test_forum_structure(self):
+        # Goes to the home page
+
+        # Sees DjangoLearners in the title and header
+
+        # There is a list of Sections on the page
+
+        # Sees Section1 and Section2 on the page
+
+        # Clicks Section1
+
+        # Is taken to a page which shows the categories in the section
+
+        # Sees Category1 and Category 2 on the page
+
+        # Clicks Category1
+
+        # Is taken to a page which shows the threads in Category1
+
+        # Sees Thread1 and Thread2
+
+        # Clicks Thread1
+
+        # Is taken to a page which shows all of the comments in Thread1
+
+        # Sees Comment1 and Comment2
         self.fail("Write it!")
