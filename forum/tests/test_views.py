cefab1776630836e055369b6fec641717a4d85b3
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-04-28 11:00:49 +0100

    Views tests now use fixtures

--- forum/tests/test_views.py
+++ forum/tests/test_views.py
@@ -4,13 +4,14 @@ from forum.models import Section
 
 class HomeViewTest(TestCase):
 
+    fixtures = ['test_forum_structure']
+
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'forum/home.html')
 
     def test_passes_sections_context_to_template(self):
-        Section.objects.create(name="A section")
         sections = Section.objects.all()
         response = self.client.get('/')
 
@@ -20,18 +21,21 @@ class HomeViewTest(TestCase):
 
 class SectionViewTest(TestCase):
 
+    fixtures = ['test_forum_structure']
+
     def test_uses_section_template(self):
-        section = Section.objects.create(name="Section")
+        section = Section.objects.first()
         response = self.client.get('/section/%d/' % (section.id,))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'forum/section.html')
 
     def test_passes_section_context_to_template(self):
-        section = Section.objects.create(name="Section1")
+        section = Section.objects.get(name="Section1")
         response = self.client.get('/section/%d/' % (section.id,))
         self.assertIn('section', response.context)
         self.assertEqual("Section1", response.context['section'].name)
 
     def test_invalid_section_id_raises_404(self):
-        response = self.client.get('/section/87/')
+        response = self.client.get('/section/87ab3/')
         self.assertEqual(response.status_code, 404)
+
