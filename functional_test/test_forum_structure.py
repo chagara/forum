1b5218c32c834200f2a127521a36c33e7764df87
d0ntreadthis <tamara.jordan1@hotmail.com>
2016-04-29 12:19:16 +0100

    Wrote helper function for clicking on items in the children table

--- functional_test/test_forum_structure.py
+++ functional_test/test_forum_structure.py
@@ -1,11 +1,22 @@
 from functional_test.base import FunctionalTest
 
 
-class ForumStructureTest(FunctionalTest):
+class BasicForumStructureTest(FunctionalTest):
 
     fixtures = ['test_forum_structure']
 
-    def test_forum_structure(self):
+    def click_child_in_children_table(self, children_table, class_, target):
+        children = children_table.find_elements_by_class_name(class_)
+        if len(children) == 0:
+            self.fail("No %ss found" % (class_,))
+        for child in children:
+            if child.text == target:
+                child.click()
+                break
+        else:
+            self.fail("%s not found" % (target,))
+
+    def test_basic_forum_structure(self):
         # Goes to the home page
         self.browser.get(self.live_server_url)
         self.assertEqual(self.browser.current_url, self.live_server_url + '/')
@@ -21,15 +32,8 @@ class ForumStructureTest(FunctionalTest):
         self.assertIn("Section2", children_table.text)
 
         # Clicks Section1
-        sections = children_table.find_elements_by_class_name("section")
-        if len(sections) == 0:
-            self.fail("No sections found")
-        for section in sections:
-            if section.text == "Section1":
-                section.click()
-                break
-        else:
-            self.fail("Section1 not found")
+        self.click_child_in_children_table(
+            children_table, "section", "Section1")
 
         # Is taken to a page which shows the categories in the section
         self.assertEqual(
@@ -37,23 +41,14 @@ class ForumStructureTest(FunctionalTest):
             self.live_server_url + '/section/1/')
         self.assertEqual(self.browser.title, "Section1")
 
-        children_table = self.browser.find_element_by_id("children")
-
         # Sees Category1 but not Category2 (it's in a different section)
+        children_table = self.browser.find_element_by_id("children")
         self.assertIn("Category1", children_table.text)
         self.assertNotIn("Category2", children_table.text)
 
         # Clicks Category1
-        categories = children_table.find_elements_by_class_name(
-            "category")
-        if len(categories) == 0 :
-            self.fail("No categories found")
-        for category in categories:
-            if category.text == "Category1":
-                category.click()
-                break
-        else:
-            self.fail("Category1 not found")
+        self.click_child_in_children_table(
+            children_table, "category", "Category1")
 
         # Is taken to a page which shows the threads in Category1
         self.assertEqual(
@@ -61,23 +56,14 @@ class ForumStructureTest(FunctionalTest):
             self.live_server_url + '/category/1/')
         self.assertEqual(self.browser.title, "Category1")
 
-        children_table = self.browser.find_element_by_id("children")
-
         # Sees Thread1 but not Thread2 (because it's in a different category)
+        children_table = self.browser.find_element_by_id("children")
         self.assertIn("Thread1", children_table.text)
         self.assertNotIn("Thread2", children_table.text)
 
         # Clicks Thread1
-        threads = children_table.find_elements_by_class_name(
-            "thread")
-        if len(threads) == 0:
-            self.fail("No threads found")
-        for Thread in threads:
-            if Thread.text == "Thread1":
-                Thread.click()
-                break
-        else:
-            self.fail("Thread1 not found")
+        self.click_child_in_children_table(
+            children_table, "thread", "Thread1")
 
         # Is taken to a page which shows all of the comments in Thread1
         self.assertEqual(
@@ -85,8 +71,7 @@ class ForumStructureTest(FunctionalTest):
             self.live_server_url + '/thread/1/')
         self.assertEqual(self.browser.title, "Thread1")
 
-        comments_table = self.browser.find_element_by_id("comments")
-
         # Sees Comment1 but not Comment2 (because it's in a different thread)
+        comments_table = self.browser.find_element_by_id("comments")
         self.assertIn("Comment1", comments_table.text)
         self.assertNotIn("Comment2", comments_table.text)
